835c13aa22c9c42cf2f6491d223a86537d63febf
Brad Crump <bradrcrump@gmail.com>
2019-11-18 17:46:54 -0800

    .

--- FinalProject/comp4980/src/main/java/com/fall2019/comp4980/Dataset.java
+++ FinalProject/comp4980/src/main/java/com/fall2019/comp4980/Dataset.java
@@ -1,21 +1,20 @@
 package com.fall2019.comp4980;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+
 import java.io.File;
 
 public class Dataset {
      private static File root;
      private static File rootDataset;
-
+     INDArray[] dataset;
      Dataset(){
           System.out.println("Initializing Dataset...");
           try {
                root = getDatasetRoot();
-
                rootDataset = new File(root.toString() + "/lfw-deepfunneled");
+               dataset = initDataset(rootDataset);
 
-               for(File file : rootDataset.listFiles()){
-                    System.out.println(file.toString());
-               }
           }
           catch(Exception e){
                System.out.println(e);
@@ -23,6 +22,15 @@ public class Dataset {
           }
           System.out.println("Dataset Initialized");
      }
+
+     private static INDArray[] initDataset(File rootDataset){
+          long numberOfClasses = rootDataset.length();
+          System.out.println(numberOfClasses);
+          for(File file : rootDataset.listFiles()){
+          }
+          return null;
+     }
+
      private static File getDatasetRoot() throws Exception{
           String currentDir = new java.io.File( "." ).getCanonicalPath();
           File datasetRoot = new File( currentDir + "/lfw-dataset" );
